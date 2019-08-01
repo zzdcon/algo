84fc26cdc2bdcb9e85ba8d6016a102ba2a52ac0c
cheney <zzdcon@163.com>
2019-08-02 07:54:58 +0800

    add 2

---
+++ src/main/java/algo/leetcode/tree/InvertTree.java
@@ -0,0 +1,51 @@
+package algo.leetcode.tree;//翻转一棵二叉树。
+//
+// 示例： 
+//
+// 输入： 
+//
+//      4
+//   /   \
+//  2     7
+// / \   / \
+//1   3 6   9 
+//
+// 输出： 
+//
+//      4
+//   /   \
+//  7     2
+// / \   / \
+//9   6 3   1 
+//
+// 备注: 
+//这个问题是受到 Max Howell 的 原问题 启发的 ： 
+//
+// 谷歌：我们90％的工程师使用您编写的软件(Homebrew)，但是您却无法在面试时在白板上写出翻转二叉树这道题，这太糟糕了。 
+//
+
+import algo.dataStructure.TreeNode;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class InvertTree {
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+        TreeNode left = root.left;
+        TreeNode right = root.right;
+        root.left = right;
+        root.right = left;
+        invertTree(root.left);
+        invertTree(root.right);
+        return root;
+    }
+}
