9a5a16f9a10e7c965a4a50428870fd8a2ac160f9
cheney <zzdcon@163.com>
2020-02-08 11:02:34 +0800

    longest common prefix

---
+++ src/main/java/algo/leetcode/str/LongestCommonPrefix.java
@@ -0,0 +1,42 @@
+package algo.leetcode.str;//编写一个函数来查找字符串数组中的最长公共前缀。
+//
+// 如果不存在公共前缀，返回空字符串 ""。 
+//
+// 示例 1: 
+//
+// 输入: ["flower","flow","flight"]
+//输出: "fl"
+// 
+//
+// 示例 2: 
+//
+// 输入: ["dog","racecar","car"]
+//输出: ""
+//解释: 输入不存在公共前缀。
+// 
+//
+// 说明: 
+//
+// 所有输入只包含小写字母 a-z 。 
+// Related Topics 字符串
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class LongestCommonPrefix {
+    public String longestCommonPrefix(String[] strs) {
+        if (strs == null || strs.length == 0 || strs[0] == null) {
+            return "";
+        }
+
+        for (int i=0; i< strs[0].length(); i++) {
+            char ch = strs[0].charAt(i);
+            for (int j=0; j< strs.length; j++) {
+                if (i == strs[j].length() || strs[j].charAt(i) != ch) {
+                    return strs[0].substring(0, i);
+                }
+            }
+        }
+        return strs[0];
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
