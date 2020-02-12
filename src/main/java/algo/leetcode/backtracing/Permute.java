a41731eba5fe8b057b112d723f052286a2ecfaeb
cheney <zzdcon@163.com>
2020-02-12 13:31:55 +0800

    multi methods for permute

--- src/main/java/algo/leetcode/backtracing/Permute.java
+++ src/main/java/algo/leetcode/backtracing/Permute.java
@@ -19,7 +19,6 @@ import java.util.*;
 
 //leetcode submit region begin(Prohibit modification and deletion)
 class Permute {
-    Map<Integer, List<List<Integer>>> map = new HashMap<>();
     public List<List<Integer>> permute(int[] nums) {
         List<List<Integer>> ans = new ArrayList<>();
         if (nums == null || nums.length == 0) {
@@ -29,16 +28,11 @@ class Permute {
     }
 
     public List<List<Integer>> partition(int index, int[] nums) {
-        if (map.get(index) != null) {
-            return map.get(index);
-        }
-
         List<List<Integer>> ans = new ArrayList<>();
         if (index == nums.length-1) {
             List<Integer> list = new ArrayList<>();
             list.add(nums[index]);
             ans.add(list);
-            map.put(index, ans);
             return ans;
         }
 
@@ -53,12 +47,78 @@ class Permute {
                 ans.add(list2);
             }
         }
-        map.put(index, ans);
         return ans;
     }
 
+
+    public List<List<Integer>> permute2(int[] nums) {
+        // init output list
+        List<List<Integer>> output = new LinkedList();
+
+        // convert nums into list since the output is a list of lists
+        ArrayList<Integer> nums_lst = new ArrayList<Integer>();
+        for (int num : nums)
+            nums_lst.add(num);
+
+        int n = nums.length;
+        backtrack(n, nums_lst, output, 0);
+        return output;
+    }
+
+    public void backtrack(int n,
+                          ArrayList<Integer> nums,
+                          List<List<Integer>> output,
+                          int first) {
+        // if all integers are used up
+        if (first == n)
+            output.add(new ArrayList<Integer>(nums));
+        for (int i = first; i < n; i++) {
+            // place i-th integer first
+            // in the current permutation
+            Collections.swap(nums, first, i);
+            // use next integers to complete the permutations
+            backtrack(n, nums, output, first + 1);
+            // backtrack
+            Collections.swap(nums, first, i);
+        }
+    }
+
+    List<List<Integer>> res = new LinkedList<>();
+
+    /* 主函数，输入一组不重复的数字，返回它们的全排列 */
+    List<List<Integer>> permute3(int[] nums) {
+        // 记录「路径」
+        LinkedList<Integer> track = new LinkedList<>();
+        backtrack(nums, track);
+        return res;
+    }
+
+    // 路径：记录在 track 中
+// 选择列表：nums 中不存在于 track 的那些元素
+// 结束条件：nums 中的元素全都在 track 中出现
+    void backtrack(int[] nums, LinkedList<Integer> track) {
+        // 触发结束条件
+        if (track.size() == nums.length) {
+            res.add(new LinkedList(track));
+            return;
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            // 排除不合法的选择
+            if (track.contains(nums[i]))
+                continue;
+            // 做选择
+            track.add(nums[i]);
+            // 进入下一层决策树
+            backtrack(nums, track);
+            // 取消选择
+            track.removeLast();
+        }
+    }
+
+
     public static void main(String[] args) {
-        System.out.println(new Permute().permute(new int[] {5,4, 6,2}));
+        System.out.println(new Permute().permute3(new int[] {1,2,3}));
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
