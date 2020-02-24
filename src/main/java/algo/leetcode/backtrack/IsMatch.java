9903fc0d099c966b5e6e076dfedce1b344d44f34
cheney <zzdcon@163.com>
2020-02-24 12:00:22 +0800

    + isMatch 2nd solution

--- src/main/java/algo/leetcode/backtrack/IsMatch.java
+++ src/main/java/algo/leetcode/backtrack/IsMatch.java
@@ -86,6 +86,41 @@ class IsMatch {
         return pLen == j;
     }
 
+
+    /**
+     * 动态规划
+     * @param s
+     * @param p
+     * @return
+     */
+    public boolean isMatch2(String s, String p) {
+        if (p == null || p.isEmpty()) {
+            return s == null || s.isEmpty();
+        }
+        int slen = s.length();
+        int plen = p.length();
+
+
+        boolean[][] dp = new boolean[slen + 1][plen + 1];
+
+        dp[0][0] = true;
+        //dp[0][1]~dp[0][p.length]只有p的j字符以及前面所有字符都为'*'才为true
+        for (int j = 1; j <= plen; j++)
+            dp[0][j] = p.charAt(j-1) == '*' && dp[0][j - 1];
+        for (int i = 1; i <= slen; i++) {
+            for (int j = 1; j <= plen; j++) {
+                char si = s.charAt(i - 1),  pj = p.charAt(j - 1);
+                if (si == pj || pj == '?') {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else if (pj == '*') {
+//                    第一种是'*'需要作为一个字母与s[i]进行匹配；第二种是'*'需要作为空字符(即不需要'*'可以直接"移除")，所以dp[i][j-1]
+                    dp[i][j] = dp[i - 1][j] || dp[i][j - 1];
+                }
+            }
+        }
+        return dp[slen][plen];
+    }
+
     public static void main(String[] args) {
         String s = "acdcb";
         String p = "a*c?b";
