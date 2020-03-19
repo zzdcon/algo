767519b18e98db916177e9c2df4fd66a8261feec
cheney <zzdcon@163.com>
2020-03-19 14:54:14 +0800

    + longestPalindrome

---
+++ src/main/java/algo/leetcode/hash/LongestPalindrome.java
@@ -0,0 +1,52 @@
+package algo.leetcode.hash;
+
+// 给定一个包含大写字母和小写字母的字符串，找到通过这些字母构造成的最长的回文串。
+//
+// 在构造过程中，请注意区分大小写。比如 "Aa" 不能当做一个回文字符串。 
+//
+// 注意: 
+//假设字符串的长度不会超过 1010。 
+//
+// 示例 1: 
+//
+// 
+//输入:
+//"abccccdd"
+//
+//输出:
+//7
+//
+//解释:
+//我们可以构造的最长的回文串是"dccaccd", 它的长度是 7。
+// 
+// Related Topics 哈希表
+
+
+import java.util.HashMap;
+import java.util.Map;
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class LongestPalindrome {
+    public int longestPalindrome(String s) {
+        if (s == null) {
+            return 0;
+        }
+        int[] count = new int[128];
+        for (char c: s.toCharArray()) {
+            count[c]++;
+        }
+        int pair = 0;
+        for (int w : count) {
+            if (w>0) {
+                pair += w/2;
+            }
+        }
+        int sum = pair * 2;
+        return s.length()>sum? sum+1:sum;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new LongestPalindrome().longestPalindrome("abccccdd"));
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
