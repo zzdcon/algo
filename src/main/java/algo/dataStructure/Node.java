17823fbce41519fdb3892325bddea7624b4e4539
cheney <zzdcon@163.com>
2020-03-25 22:34:51 +0800

    + surfaceArea

---
+++ src/main/java/algo/dataStructure/Node.java
@@ -0,0 +1,39 @@
+package algo.dataStructure;
+
+public class Node {
+    public int val;
+    public Node left;
+    public Node right;
+    public Node next;
+
+    public Node() {}
+    
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, Node _left, Node _right, Node _next) {
+        val = _val;
+        left = _left;
+        right = _right;
+        next = _next;
+    }
+
+    @Override
+    public String toString() {
+        return "Node{" +
+                "val=" + val +
+                ", left=" + left +
+                ", right=" + right +
+                ", next=" + next +
+                '}';
+    }
+
+//    public static Node build(int[] data) {
+//        if (data.length == 0) {
+//            return null;
+//        }
+//
+//        Node node = new Node(data)
+//    }
+}
