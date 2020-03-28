5050f9a7681f1237051b60599380c4c134ce46be
cheney <zzdcon@163.com>
2020-03-28 10:21:52 +0800

    + matPathSum

---
+++ src/main/java/algo/leetcode/tree/MaxPathSum.java
@@ -0,0 +1,74 @@
+package algo.leetcode.tree;
+
+//给定一个非空二叉树，返回其最大路径和。
+//
+// 本题中，路径被定义为一条从树中任意节点出发，达到任意节点的序列。该路径至少包含一个节点，且不一定经过根节点。 
+//
+// 示例 1: 
+//
+// 输入: [1,2,3]
+//
+//       1
+//      / \
+//     2   3
+//
+//输出: 6
+// 
+//
+// 示例 2: 
+//
+// 输入: [-10,9,20,null,null,15,7]
+//
+//   -10
+//   / \
+//  9  20
+//    /  \
+//   15   7
+//
+//输出: 42 
+// Related Topics 树 深度优先搜索
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+
+import algo.dataStructure.TreeNode;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class MaxPathSum {
+    int max = Integer.MIN_VALUE;
+    public int maxPathSum(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        dfs(root);
+        return max;
+    }
+
+    /**
+     * 返回经过root的单边分支最大和， 即Math.max(root, root+left, root+right)
+     * @param root
+     * @return
+     */
+    public int dfs(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        //计算左边分支最大值，左边分支如果为负数还不如不选择
+        int leftMax = Math.max(0, dfs(root.left));
+        //计算右边分支最大值，右边分支如果为负数还不如不选择
+        int rightMax = Math.max(0, dfs(root.right));
+        //left->root->right 作为路径与历史最大值做比较
+        max = Math.max(max, root.val + leftMax + rightMax);
+        // 返回经过root的单边最大分支给上游
+        return root.val + Math.max(leftMax, rightMax);
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
