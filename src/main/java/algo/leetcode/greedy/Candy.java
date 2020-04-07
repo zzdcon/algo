be03597984e440dee46686f5c05ad3b2e668a9da
cheney <zzdcon@163.com>
2020-04-07 21:25:36 +0800

    modify candy

--- src/main/java/algo/leetcode/greedy/Candy.java
+++ src/main/java/algo/leetcode/greedy/Candy.java
@@ -31,6 +31,12 @@ import java.util.Arrays;
 
 //leetcode submit region begin(Prohibit modification and deletion)
 class Candy {
+    /**
+     * 方法一： 暴力破解
+     *
+     * @param ratings
+     * @return
+     */
     public int candy(int[] ratings) {
         if (ratings.length == 0) return 0;
         int[] ans = new int[ratings.length];
@@ -38,30 +44,63 @@ class Candy {
         boolean flag = true;
         while (flag) {
             flag = false;
-            for (int i=1; i<ratings.length;) {
-                if (ratings[i] > ratings[i-1] && ans[i] <= ans[i - 1]) {
+            for (int i = 1; i < ratings.length; ) {
+                if (ratings[i] > ratings[i - 1] && ans[i] <= ans[i - 1]) {
                     ans[i]++;
                     flag = true;
                 }
-                if (ratings[i] < ratings[i-1] && ans[i] >= ans[i-1]) {
-                    ans[i-1]++;
+                if (ratings[i] < ratings[i - 1] && ans[i] >= ans[i - 1]) {
+                    ans[i - 1]++;
                     flag = true;
                 }
                 i++;
             }
         }
         int res = 0;
-        for (int i=0; i<ans.length; i++) {
+        for (int i = 0; i < ans.length; i++) {
             res += ans[i];
         }
         return res;
     }
 
+    /**
+     * 1.新建一个数组保存每个孩子的糖果数，开始全部给每个小孩都发一个糖；
+     * 2.第一次遍历先从左向右 若右边的孩子评分比左边高 则右边的糖果数等于左边的糖果数加一
+     * 3.第二次遍历先从右向左 若左边的孩子评分比右边高且左边的孩子糖果数小于等于右边的 则左边的糖果数等于右 边的糖果数加一 同时求和
+     *
+     * @param ratings
+     */
+    public int candy2(int[] ratings) {
+        if (ratings.length == 0) return 0;
+        int[] candies = new int[ratings.length];
+        Arrays.fill(candies, 1);
+        for (int i=1; i<ratings.length; i++) {
+            if (ratings[i] > ratings[i-1]) {
+                candies[i] = candies[i-1] + 1;
+            }
+        }
+
+        for (int i=ratings.length-1; i>0; i--) {
+            if (ratings[i] < ratings[i-1] && candies[i-1]<=candies[i]) {
+                candies[i-1] = candies[i]+1;
+            }
+        }
+
+        int sum = 0;
+        for (int i=0; i<candies.length; i++) {
+            sum += candies[i];
+        }
+
+        return sum;
+    }
+
+
     public static void main(String[] args) {
-        System.out.println(new Candy().candy(new int[]{1,6,10,8,7,3,2}));
-        System.out.println(new Candy().candy(new int[]{1,2,87,87,87,2,1}));
-        System.out.println(new Candy().candy(new int[]{1,0,2}));
-        System.out.println(new Candy().candy(new int[]{1,2,2}));
+        System.out.println(new Candy().candy(new int[]{1, 6, 10, 8, 7, 3, 2}));
+        System.out.println(new Candy().candy2(new int[]{1, 6, 10, 8, 7, 3, 2}));
+        System.out.println(new Candy().candy2(new int[]{1, 2, 87, 87, 87, 2, 1}));
+        System.out.println(new Candy().candy2(new int[]{1, 0, 2}));
+        System.out.println(new Candy().candy2(new int[]{1, 2, 2}));
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
