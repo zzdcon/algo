f75d32e5eca6544b11bccd4d53a16239af37a180
cheney <zzdcon@163.com>
2020-04-07 14:03:54 +0800

    + candy

---
+++ src/main/java/algo/leetcode/greedy/Candy.java
@@ -0,0 +1,73 @@
+package algo.leetcode.greedy;
+
+//老师想给孩子们分发糖果，有 N 个孩子站成了一条直线，老师会根据每个孩子的表现，预先给他们评分。
+//
+// 你需要按照以下要求，帮助老师给这些孩子分发糖果： 
+//
+// 
+// 每个孩子至少分配到 1 个糖果。 
+// 相邻的孩子中，评分高的孩子必须获得更多的糖果。 
+// 
+//
+// 那么这样下来，老师至少需要准备多少颗糖果呢？ 
+//
+// 示例 1: 
+//
+// 输入: [1,0,2]
+//输出: 5
+//解释: 你可以分别给这三个孩子分发 2、1、2 颗糖果。
+// 
+//
+// 示例 2: 
+//
+// 输入: [1,2,2]
+//输出: 4
+//解释: 你可以分别给这三个孩子分发 1、2、1 颗糖果。
+//     第三个孩子只得到 1 颗糖果，这已满足上述两个条件。 
+// Related Topics 贪心算法
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Candy {
+    public int candy(int[] ratings) {
+        if (ratings.length == 0) return 0;
+        int[] ans = new int[ratings.length];
+        for (int i=0; i<ratings.length; i++) {
+            // 预先分配没人一个
+            ans[i] = 1;
+        }
+        while (!checkFinish(ratings, ans)) {
+            for (int i=1; i<ratings.length;) {
+                while (ratings[i] > ratings[i-1] && ans[i] <= ans[i - 1]) {
+                    ans[i]++;
+                }
+                while (ratings[i] < ratings[i-1] && ans[i] >= ans[i-1]) {
+                    ans[i-1]++;
+                }
+                i++;
+            }
+        }
+        int res = 0;
+        for (int i=0; i<ans.length; i++) {
+            res += ans[i];
+        }
+        return res;
+    }
+
+    boolean checkFinish(int[] ratings, int[] ans) {
+        for (int i=1; i<ratings.length; i++) {
+            if (ratings[i] != ratings[i-1]) {
+                if ((ratings[i]-ratings[i-1])*(ans[i]-ans[i-1]) <=0) return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Candy().candy(new int[]{1,6,10,8,7,3,2}));
+        System.out.println(new Candy().candy(new int[]{1,2,87,87,87,2,1}));
+        System.out.println(new Candy().candy(new int[]{1,0,2}));
+        System.out.println(new Candy().candy(new int[]{1,2,2}));
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
